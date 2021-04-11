63dd4ccdd7378570dba4aefb8494bf9e9230a8d0
HHL008 <qhhhh08@gmail.com>
2021-04-11 10:26:39 -0400

    updated the html and css with Js to create Burger menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 (() => {
   //stub
   console.log('JS is linked! Hooray!');
+
+let burger = document.querySelector('#burger'),
+      burgerContent = document.querySelector('#mainNav'),
+      navFiller = document.querySelector('#mobileNavCon'),
+      btns = document.querySelectorAll('a');
+
+      btns.forEach(btn => {
+        btn.addEventListener('click', function(){
+          btns.forEach(btn => btn.classList.remove('active'));
+          this.classList.add('active');
+        })
+      })
+
+
+      function burgerMenu() {
+        burgerContent.classList.toggle("slideToggle");
+        navFiller.classList.toggle("blank");
+      };
+
+      burger.addEventListener("click", burgerMenu, false);
+
 })();
